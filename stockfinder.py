b0a956071279f8e039af4ce91e9e3effa5dd2476
Tron <zoniixyt@gmail.com>
2020-12-19 02:48:11 -0500

    Trying to add functionality for Selenium. Done for the night

--- stockfinder.py
+++ stockfinder.py
@@ -1,27 +1,27 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
-import colorama
-from colorama import Fore, Back, Style
 import requests
 
-colorama.init(autoreset=True)
 
 newegg_products = {
-    "Newegg Ryzen 5 5600x": "https://www.newegg.com/amd-ryzen-5-5600x/p/N82E16819113666?Description=ryzen&cm_re=ryzen-_-19-113-666-_-Product",
-    "Newegg Ryzen 7 5800x": "https://www.newegg.com/amd-ryzen-7-5800x/p/N82E16819113665?Description=ryzen%205700&cm_re=ryzen_5700-_-19-113-665-_-Product",
-    "Newegg Ryzen 9 5900x": "https://www.newegg.com/amd-ryzen-9-5900x/p/N82E16819113664?Description=ryzen%205700&cm_re=ryzen_5700-_-19-113-664-_-Product",
-    "Newegg Ryzen 9 5950x": "https://www.newegg.com/amd-ryzen-9-5900x/p/N82E16819113664?Description=ryzen%205700&cm_re=ryzen_5700-_-19-113-664-_-Product"
+    "NeweggUS Ryzen 5 5600x": "https://www.newegg.com/amd-ryzen-5-5600x/p/N82E16819113666?Description=ryzen&cm_re=ryzen-_-19-113-666-_-Product",
+    "NeweggUS Ryzen 7 5800x": "https://www.newegg.com/amd-ryzen-7-5800x/p/N82E16819113665?Description=ryzen%205700&cm_re=ryzen_5700-_-19-113-665-_-Product",
+    "NeweggUS Ryzen 9 5900x": "https://www.newegg.com/amd-ryzen-9-5900x/p/N82E16819113664?Description=ryzen%205700&cm_re=ryzen_5700-_-19-113-664-_-Product",
+    "NeweggUS Ryzen 9 5950x": "https://www.newegg.com/amd-ryzen-9-5900x/p/N82E16819113664?Description=ryzen%205700&cm_re=ryzen_5700-_-19-113-664-_-Product"
 }
 
 amazon_products = {
-    "Amazon Ryzen 5 5600x": "https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF?ref_=ast_sto_dp",
-    "Amazon Ryzen 7 5800x": "https://www.amazon.com/AMD-Ryzen-5800X-16-Thread-Processor/dp/B0815XFSGK?ref_=ast_sto_dp",
-    "Amazon Ryzen 9 5900x": "https://www.amazon.com/Ryzen-5900X-12-Core-Desktop-Processor/dp/B08NXYLBN5/ref=sr_1_3?dchild=1&keywords=ryzen+9+5900x&qid=1608348416&s=electronics&sr=1-3",
-    "Amazon Ryzen 9 5950x": "https://www.amazon.com/AMD-Ryzen-5950X-32-Thread-Processor/dp/B0815Y8J9N?ref_=ast_sto_dp"
+    "AmazonUS Ryzen 5 5600x": "https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF?ref_=ast_sto_dp",
+    "AmazonUS Ryzen 7 5800x": "https://www.amazon.com/AMD-Ryzen-5800X-16-Thread-Processor/dp/B0815XFSGK?ref_=ast_sto_dp",
+    "AmazonUS Ryzen 9 5900x": "https://www.amazon.com/Ryzen-5900X-12-Core-Desktop-Processor/dp/B08NXYLBN5/ref=sr_1_3?dchild=1&keywords=ryzen+9+5900x&qid=1608348416&s=electronics&sr=1-3",
+    "AmazonUS Ryzen 9 5950x": "https://www.amazon.com/AMD-Ryzen-5950X-32-Thread-Processor/dp/B0815Y8J9N?ref_=ast_sto_dp"
 }
 
 bestbuy_products = {
-
+    "BestBuyUS Ryzen 5 5600x": "https://www.bestbuy.com/site/amd-ryzen-5-5600x-4th-gen-6-core-12-threads-unlocked-desktop-processor-with-wraith-stealth-cooler/6438943.p?skuId=6438943"
+    "BestBuyUS Ryzen 7 5800x": "https://www.bestbuy.com/site/amd-ryzen-7-5800x-4th-gen-8-core-16-threads-unlocked-desktop-processor-without-cooler/6439000.p?skuId=6439000"
+    "BestBuyUS Ryzen 9 5900x": "https://www.bestbuy.com/site/amd-ryzen-9-5900x-4th-gen-12-core-24-threads-unlocked-desktop-processor-without-cooler/6438942.p?skuId=6438942"
+    "BestBuyUS Ryzen 9 5950x": "https://www.bestbuy.com/site/amd-ryzen-9-5950x-4th-gen-16-core-32-threads-unlocked-desktop-processor-without-cooler/6438941.p?skuId=6438941"
 }
 
 
@@ -58,20 +58,20 @@ while repeat == True:
     for key in newegg_products:
         if findNeweggStock(newegg_products[key]) == 0:
             now = datetime.now()
-            print(key, "Out of Stock", Fore.RED + "  UPDATED:",
+            print(key, "Out of Stock", "  UPDATED:",
                   now.strftime("%m/%d/%Y %H:%M:%S"))
         else:
             now = datetime.now()
-            print(key, "In Stock", Fore.RED + "  UPDATED:",
+            print(key, "In Stock", "  UPDATED:",
                   now.strftime("%m/%d/%Y %H:%M:%S"))
 
     for key in amazon_products:
         if findAmazonStock(amazon_products[key]) == 0:
             now = datetime.now()
-            print(key, "Out of Stock", Fore.RED + "  UPDATED:",
+            print(key, "Out of Stock", "  UPDATED:",
                   now.strftime("%m/%d/%Y %H:%M:%S"))
         else:
             now = datetime.now()
-            print(key, "In stock", Fore.RED + "  UPDATED:",
+            print(key, "In stock", "  UPDATED:",
                   now.strftime("%m/%d/%Y %H:%M:%S"))
     repeat = True
