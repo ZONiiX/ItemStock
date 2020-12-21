ec49d98434102634644b64522520602ae3e2191b
Tron <zoniixyt@gmail.com>
2020-12-21 12:53:00 -0500

    Removed unused assets

--- amazon_stock.py
+++ amazon_stock.py
@@ -7,8 +7,6 @@ colorama.init(autoreset=True)
 amazon_products = ['https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF?ref_=ast_sto_dp', 'https://www.amazon.com/AMD-Ryzen-5800X-16-Thread-Processor/dp/B0815XFSGK?ref_=ast_sto_dp',
                    'https://www.amazon.com/Ryzen-5900X-12-Core-Desktop-Processor/dp/B08NXYLBN5/ref=sr_1_3?dchild=1&keywords=ryzen+9+5900x&qid=1608348416&s=electronics&sr=1-3', 'https://www.amazon.com/AMD-Ryzen-5950X-32-Thread-Processor/dp/B0815Y8J9N?ref_=ast_sto_dp']
 
-#amazon_products = ['https://www.amazon.com/AMD-Ryzen-3600-12-Thread-Processor/dp/B07STGGQ18/ref=pd_di_sccai_5?pd_rd_w=aSJmR&pf_rd_p=c9443270-b914-4430-a90b-72e3e7e784e0&pf_rd_r=TSZX18GNQ772059XTBGV&pd_rd_r=18eb92e9-2a10-4fda-b68d-f28693f70a62&pd_rd_wg=62Cxv&pd_rd_i=B07STGGQ18&psc=1']
-
 
 def getAmazonStock(url, header):
     s = HTMLSession()
@@ -60,6 +58,3 @@ def loopAmazonStock():
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
         getAmazonStock(url, headers)
-
-
-loopAmazonStock()
